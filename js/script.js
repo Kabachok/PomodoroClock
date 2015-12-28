1013b83e4fb36ce230c5afb2bf3e48bb1b2a6a10
Daria <koumori-daria@mail.ru>
2015-12-28 14:05:37 +0400

    basic animation of the timer
    Style was changed, but still needs in improvement. Basic animation
    added.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // program code for the timer
 
 // how to anime a progress bar http://www.hongkiat.com/blog/html5-progress-bar/
+
+$(document).ready(function(){
+	$(".start-pause").click(function(){
+		changePosition();
+	});
+});
+
+function changePosition(){
+	$(".timer-fill").animate({top:'+=175px'}, 60000).animate({top:'-=175px'}, 60000, changePosition);
+}
