fa9d9ee9439f39b2178782f1bb7b3c193ae6353a
Daria <koumori-daria@mail.ru>
2015-12-29 19:45:22 +0400

    TODO's are added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 	$(".start-pause").click(function(){
+		// TODO: change functionality to click on the timer for toggling the class with css animation
 		changePosition();
 	});
+	// TODO: implement timer functionality
+
+	// TODO: dinamically change the value of the timer clock
+
+	// TODO: set both timers to the value from the SESSION and BREAK areas (has to be implemented in html file)
+
+	// TODO: implement the code for the RESET button
+	// it has to set all values to their defaults and stop the timer if it was started
 });
 
 // TODO: refact with this idea: http://codepen.io/gabrieleromanato/pen/jEfbn?editors=011
